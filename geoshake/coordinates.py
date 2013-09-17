60e12a3ce00104debb6a2813f7cc96fc036f3c31
Damien Riquet <damien.riquet@lifl.fr>
2013-09-17 09:19:34 +0200

    Initial commit

---
+++ geoshake/coordinates.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+File: coordinates.py
+Author: Damien Riquet <d.riquet@gmail.com>
+Description: Deals with GPS coordinates
+'''
+
+from math import radians, cos, sin, asin, sqrt, floor, fabs
+
+class DecimalDegrees:
+    """
+        Decimal degrees coordinates
+        Composed of two values : latitude and longitude
+    """
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
+
+    def __repr__(self):
+        return '%f %f' % (self.lat, self.lon)
+
+    def to_geocaching_string(self):
+        # Determining N/S and E/W
+        lat_pos = 'N' if self.lat >= 0 else 'S'
+        lon_pos = 'E' if self.lon >= 0 else 'W'
+
+        lat = fabs(self.lat)
+        lon = fabs(self.lon)
+
+        # Computing degrees
+        lat_deg = floor(lat)
+        lon_deg = floor(lon)
+
+        # Computing minutes
+        lat_min = (lat - lat_deg) * 60
+        lon_min = (lon - lon_deg) * 60
+
+        return '%s%d°%.3f %s%d°%.3f' % (lat_pos, lat_deg, lat_min, lon_pos, lon_deg, lon_min)
+
+    def distance_to(self, other):
+        """
+            Compute the distance between two coordinates
+            Use the haversine formula to calculate the great-circle distance between two points
+        """
+        # convert decimal degrees to radians
+        lon1, lat1, lon2, lat2 = map(radians, [self.lon, self.lat, other.lon, other.lat])
+        # haversine formula
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+        c = 2 * asin(sqrt(a))
+        km = 6371 * c
+        return km
+
+
+
+
+
+def geocaching2decimal(lat, lon):
+    """
+        Create decimal degrees coordinates from geocaching coordinates
+        Geocaching coordinates are formated as follow:
+            N50°40.232 E3°03.232
+        lat and lon are tuples:
+            (sign, degree, minutes)
+        usage:
+            geocaching2decimal((1, 50, 40.232), (1, 3, 3.232))
+    """
+    lat = lat[0] * (lat[1] + lat[2]/60)
+    lon = lon[0] * (lon[1] + lon[2]/60)
+    return DecimalDegrees(lat, lon)
+
+
+
+if __name__ == '__main__':
+    a = geocaching2decimal((1, 41, 36.455), (-1, 88, 12.221))
+    b = geocaching2decimal((1, 35, 8.073), (-1, 85, 21.505))
+
+    print a
+    print b
+
+    print a.distance_to(b)
+
+    a = DecimalDegrees(41.607583, -88.203683)
+    b = DecimalDegrees(41.650667, -88.255467)
+
+    print a
+    print b
+
+    print a.distance_to(b)
+
---
+++ geoshake/geoshake.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+File: coordinates.py
+Author: Damien Riquet <d.riquet@gmail.com>
+Description: Deals with GPS coordinates
+'''
+
+import itertools
+import coordinates
+
+class UnknownValue:
+    """ Represents an unknown value """
+    def __init__(self, symbol, bound_min, bound_max):
+        self.symbol = symbol
+        self.bound_min = bound_min
+        self.bound_max = bound_max
+
+    def __iter__(self):
+        return iter(xrange(self.bound_min, self.bound_max + 1))
+
+    def __len__(self):
+        return self.bound_max - self.bound_min + 1
+
+
+def generate_solutions(origin, coord, unknowns, max_distance=2):
+    solutions = []
+    for combination in itertools.product(*unknowns):
+        substitutions = [(unknowns[i].symbol, combination[i]) for i in xrange(len(combination))]
+
+        attempt_lat = [substitute(elt, substitutions) for elt in coord[0]]
+        attempt_lon = [substitute(elt, substitutions) for elt in coord[1]]
+
+        # Verify that values are valid
+        if valid_coordinates(attempt_lat, attempt_lon):
+            attempt_lat = (attempt_lat[0], attempt_lat[1], float('%d.%d' % (attempt_lat[2], attempt_lat[3])))
+            attempt_lon = (attempt_lon[0], attempt_lon[1], float('%d.%d' % (attempt_lon[2], attempt_lon[3])))
+
+            attempt = coordinates.geocaching2decimal(attempt_lat, attempt_lon)
+
+            d = origin.distance_to(attempt)
+            if d <= max_distance:
+                solutions.append((combination, attempt))
+        else:
+            print 'invalid', attempt_lat, attempt_lon
+
+
+    return solutions
+
+
+def substitute(coord_value, substitutions):
+    str_value = str(coord_value)
+    for symbol, value in substitutions:
+        str_value = str_value.replace(symbol, str(value))
+    return int(str_value)
+
+def valid_coordinates(lat, lon):
+    return value_between(lat[1], 0, 99) and \
+       value_between(lat[2], 0, 99) and \
+       value_between(lat[3], 0, 999) and \
+       value_between(lon[1], 0, 999) and \
+       value_between(lon[2], 0, 99) and \
+       value_between(lon[3], 0, 999)
+
+
+def value_between(val, val_min, val_max):
+    return val >= val_min and val <= val_max
+
+
+if __name__ == '__main__':
+    a = UnknownValue('A', 0, 8)
+    b = UnknownValue('B', 4, 8)
+    c = UnknownValue('C', 0, 9)
+
+    coord = ((1, '50', '40', 'AB4'), (1, 'A', '40', 'CC4'))
+    orig = coordinates.geocaching2decimal((1, 50, 40.500), (1, 3, 40.225))
+
+
+    total = len(a) * len(b) * len(c)
+    print len(generate_solutions(orig, coord, [a,b,c])), total
