159a198a3540b2764521514176f7eb43d65b50c9
Damien Riquet <damien.riquet@lifl.fr>
2013-09-23 16:44:57 +0200

    work on templates + forms

--- geoshake/frontend/forms.py
+++ geoshake/frontend/forms.py
@@ -6,11 +6,52 @@ Author: Damien Riquet <d.riquet@gmail.com>
 Description: Web forms
 '''
 
+import re
 from wtforms import Form, TextField, FormField, IntegerField, FieldList, validators
 
 
+# Mystery regexp
+# It must be valid coordinates
+# Format example : N50 40.000 E003 10.000
+mystery_re = re.compile(
+    "\s*"
+    "(?P<lat_pos>(?:N|S))" # Latitude position
+    "\s*"
+    "(?P<lat_deg>\d{1,2})" # Latitude degrees
+    "\s+" # At least one space
+    "(?P<lat_min>\d{1,2}\.\d{1,3})" # Latitude minutes
+    "\s+" # At least one space
+    "(?P<lon_pos>(?:E|W))" # Latitude position
+    "\s*"
+    "(?P<lon_deg>\d{1,3})" # Longitude degrees
+    "\s+" # At least one space
+    "(?P<lon_min>\d{1,2}\.\d{3})" # Longitude minutes
+    "$"
+)
+
+
+# Cache regexp
+# It must be valid coordinates but it can contains symbol like A-Z
+# Format example : N50 40.A00 E3 10.00A
+cache_re = re.compile(
+    "\s*"
+    "(?P<lat_pos>(?:N|S))" # Latitude position
+    "\s*"
+    "(?P<lat_deg>(?:\d|[A-Z]){1,2})" # Latitude degrees
+    "\s+" # At least one space
+    "(?P<lat_min>(?:\d|[A-Z]){1,2}\.(?:\d|[A-Z]){1,3})" # Latitude minutes
+    "\s+" # At least one space
+    "(?P<lon_pos>(?:E|W))" # Latitude position
+    "\s*"
+    "(?P<lon_deg>(?:\d|[A-Z]){1,3})" # Longitude degrees
+    "\s+" # At least one space
+    "(?P<lon_min>(?:\d|[A-Z]){1,2}\.(?:\d|[A-Z]){3})" # Longitude minutes
+    "$"
+)
+
+
 class UnknownForm(Form):
-    symbol = TextField('Symbol', [], default="A")
+    symbol = TextField('Symbol', [validators.required()], default="A")
     min_val = IntegerField('Minimum value', [], default=0)
     max_val = IntegerField('Minimum value', [], default=10)
 
@@ -24,9 +65,21 @@ class ShakerForm(Form):
             - up to six unknowns with boundaries
     """
     # Origin and mystery data
-    mystery = TextField('Mystery coordinates', [validators.required()], default="N50 40.000 E003 10.000")
-    cache = TextField('Cache coordinates', [validators.required()], default="N50 40.A00 E3 10.00A")
-    max_distance = IntegerField('Max distance (km)', [validators.required(), validators.NumberRange(min=1)], default=2)
+    mystery = TextField('Mystery coordinates', [
+        validators.Regexp(mystery_re, message="Wrong coordinates format.")
+    ], default="N50 40.000 E003 10.000")
+    cache = TextField('Cache coordinates', [
+        validators.Regexp(cache_re, message="Wrong coordinates format.")
+    ], default="N50 40.A00 E3 10.00A")
+    max_distance = IntegerField('Max distance (km)', [
+        validators.NumberRange(min=1, message="Must be greater or equal to %(min)d."),
+    ], default=2)
 
-    # Unknown values forms
-    variables = FieldList(FormField(UnknownForm), min_entries=6, max_entries=6)
+
+if __name__ == '__main__':
+    coord = "N50 40.A00 E3 10.00A"
+
+    m = mystery_re.match(coord)
+    if m:
+        print 'MATCH'
+        print m.groups()
--- geoshake/frontend/views.py
+++ geoshake/frontend/views.py
@@ -6,9 +6,10 @@ app.secret_key = 'blablabla'
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     form = forms.ShakerForm(request.form)
+    variables = [forms.UnknownForm(request.form)] * 6
     if request.method == 'POST' and form.validate():
         print 'validate !'
-    return render_template('home.html', form=form)
+    return render_template('home.html', form=form, variables=variables)
 
 @app.route('/static_files/<path:filename>')
 def static_files(filename):
