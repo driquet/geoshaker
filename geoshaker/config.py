d30d7df0bf3bba3e7d650d0b1addcf037080af83
Damien Riquet <damien.riquet@lifl.fr>
2013-10-08 14:51:58 +0200

    Babel (config) + base html tpl

--- geoshaker/config.py
+++ geoshaker/config.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Front end related data
 FRONTEND_HOST = '0.0.0.0'
 FRONTEND_PORT = 8080
@@ -7,3 +9,9 @@ CONTACT_MAIL = 'tobeedited@pwet.com'
 # Misc
 SECRET_KEY = '<your_secret_(random_string)>'
 DEBUG = True
+
+# Languages
+LANGUAGES = {
+    'en': 'English',
+    'fr': 'Français'
+}
--- geoshaker/run_frontend.py
+++ geoshaker/run_frontend.py
@@ -11,7 +11,8 @@ Run the web frontend
 '''
 
 import os
-from flask import Flask
+from flask import Flask, request
+from flask.ext.babel import Babel
 from geoshaker.frontend import views
 
 
@@ -21,6 +22,9 @@ app.debug = True
 app.config.from_pyfile(os.path.join(os.getcwd(), 'config.py'))
 app.register_blueprint(views.frontend, url_prefix=app.config['FRONTEND_PREFIX'])
 
+# Language/Babel
+babel = Babel(app)
+
 
 @app.context_processor
 def utility_processor():
@@ -28,6 +32,12 @@ def utility_processor():
         var_symbols = [elt[0] for elt in variables]
         return ', '.join(['%s = %d' % elt for elt in zip(var_symbols, combination)])
     return dict(format_variables=format_variables)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
+
 
 def main():
     app.run(host=app.config['FRONTEND_HOST'], port=app.config['FRONTEND_PORT'])
